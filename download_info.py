633c807fb0720811fc60d4f5a3a67d0cad3537fa
jim chen <chenzemingczm@163.com>
2018-07-02 09:51:02 +0800

    k886.net看漫画网站抓取爬虫

---
+++ download_info.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+import time
+from bs4 import BeautifulSoup
+import download_pictures
+
+
+def download_info():
+    """ 下载列表页（包含所有对图片的描述信息），并存储到data/info.txt文件中 """
+    chapter_list = []
+
+    url = 'https://m.k886.net/comic/name/17種性幻想情侶遊戲/id/33109'
+    rsp = requests.get(url)
+    soup = BeautifulSoup(rsp.content, "html.parser")
+    li_tag = soup.find("div", {"id": "chapterList"}).find_all("li")
+    for li in li_tag:
+        a = li.find("a")
+        if a:
+            chapter_list.append({"link": a["href"], "title": a["title"]})
+
+    has_chapter_list = download_pictures.get_link_info()
+    for chapter in chapter_list:
+        if chapter["link"] in has_chapter_list:
+            continue
+        link = chapter["link"]
+        chapter["list"] = []
+        while link is not None:
+            page_img, next_url = download_page(link)
+            if page_img is None:
+                time.sleep(5)
+            else:
+                chapter["list"].append(page_img)
+                if next_url is not None:
+                    link = next_url
+                else:
+                    break
+
+        save_page(chapter)
+
+
+def download_page(url):
+    """ 下载某页面的信息 """
+    page_img = None
+    rsp = requests.get(url)
+    if len(rsp.content) == 0:
+        return None, None
+    soup = BeautifulSoup(rsp.content, "html.parser")
+    img_tag = soup.find("div", {"id": "manga"}).find_all("img")
+    for img in img_tag:
+        if img.get("alt", None) is not None:
+            page_img = img["src"]
+            break
+
+    next_url = None
+    li_tag = soup.find("div", {"id": "action"}).find_all("li")
+    for li in li_tag:
+        a = li.find("a")
+        if a and a.text == u"下一頁" and not a["href"].startswith("javascript"):
+            next_url = a["href"]
+            break
+
+    return page_img, next_url
+
+
+def save_page(chapter):
+    """ 保存某页面的信息 """
+    txt = json.dumps(chapter)
+    with open('data/info.txt', 'a') as f:
+        f.write(txt)
+        f.write('\n')
+
+
+if __name__ == "__main__":
+    download_info()
---
+++ generate_html.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import os
+from download_pictures import get_info, get_info_imgs, download
+
+def generate_html(imgs):
+    """ 生成html """
+    content = {}
+    for img in imgs:
+        _, _, filepath, title = img
+        if title in content:
+            content[title] += u'<img src="' + filepath[5:] + u'" /><br />'
+        else:
+            content[title] = u'<img src="' + filepath[5:] + u'" /><br />'
+
+    items = content.items()
+    items.sort()
+    prev_chapter = "javascript:;"
+    for title, chapter in items:
+        content[title] = u'<html><head><title>' + title + u'</title></head><body>' + chapter + u'<a style="font-size:18px;" href="' + prev_chapter + u'">上一页</a>'
+        prev_chapter = title + '.html'
+    items = content.items()
+    items.sort(reverse=True)
+    next_chapter = "javascript:;"
+    for title, chapter in items:
+        content[title] = chapter + u'<a style="font-size:18px;" href="' + next_chapter + u'">下一页</a></body></html>'
+        next_chapter = title + '.html'
+    for title, chapter in content.iteritems():
+        html = os.path.join("data", title + '.html')
+        with open(html, 'wb') as f:
+            f.write(chapter.encode("gbk"))
+            print('end generate', title)
+
+if __name__ == '__main__':
+    info = get_info()
+    imgs = get_info_imgs(info)
+    generate_html(imgs)
